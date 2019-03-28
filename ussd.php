1c1c8ffc1d8df281f95e00a42fc26d2201375d9e
hesbornO <oswago54@gmail.com>
2019-03-28 22:38:01 +0300

    more comments on code

--- ussd.php
+++ ussd.php
@@ -1,4 +1,8 @@
 <?php
+//Oswago Hesborn
+//University of Nairobi
+//Reg: P15/44220/2017
+
 
 // Reads the variables sent via POST from our gateway
 $sessionId   = $_POST["sessionId"];
@@ -6,23 +10,23 @@ $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
 $text        = $_POST["text"];
 
-//user entry is stored in text
+//user input is stored in text
 
-if ( $text == "" ) {
+if ( $text == "" ) {                              //first message displayed on submission of USSD code
 
-	 $response  = "CON Enter Username and Email Separated by Hash \nExample: yourusername#username@mail.com";	//first message displayed on submission of USSD code
+	 $response  = "CON Enter Username and Email Separated by Hash \nExample: yourusername#username@mail.com";	
 
-}else if(strpos($text,'#') !== false)										//make sure user input has #
+}else if(strpos($text,'#') !== false)						  //make sure user input has # to separate the two items
 {
-  $split = explode("#",$text);												//user input is separated by #
-  $username = $split[0];													//first part is the username
-  $email = $split[1];														//second part is the email
+  $split = explode("#",$text);										//user input is separated by # and stored in split variable
+  $username = $split[0];													//first value of split is the username
+  $email = $split[1];													  	//second value of split is the email
 
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {							//email validation
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {							//email validation using in-built function
 	  $response = "END Invalid email format! \nRevalidate your email \nEmail should be after hash sign.";
 	}else{
-    $response = "END Congratulations $username! You have been successfully registered.\n\n";
-    $response .= "Your Details are \n Username: $username\nEmail:$email\nPhone: $phoneNumber";			//display user details
+    $response = "END Congratulations $username! You have been successfully registered.\n\n";     //registration notification   
+    $response .= "Your Details are \n Username: $username\nEmail:$email\nPhone: $phoneNumber";	  //display user details
   }
 
 }
@@ -31,7 +35,9 @@ if ( $text == "" ) {
 header('Content-type: text/plain');
 echo $response;  	//display response
 
-// DONE!!!
+// SHORT NOTES
+//This program uses an FTP(FileZilla) server whose access I got from a friend of mine
+//an external server is unnecessary for the USSD code to work
 ?>
 
 
